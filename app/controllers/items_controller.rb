3842da317cf24650ac1b2647e473cc743b72b5b4
Vivian Bow <vivianbao1009@gmail.com>
2021-11-16 17:00:30 +0900

    Checking

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,11 @@ class ItemsController < ApplicationController
   def index
     # @items = policy_scope(Item)
     @items = policy_scope(Item)
+    authorize @items
   end
 
   def show
     @item = Item.find(params[:id])
+    authorize @item
   end
 end
