f6afeb89dc5505aaec738f720d57b7cb6cf6a3cf
Caio Santos <caio.santos@msn.com>
2021-11-16 14:51:14 +0900

    users, items and offerings ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+puts puts 'Cleaning up database...'
+Offering.destroy_all
+User.destroy_all
+Item.destroy_all
+puts 'Database cleaned'
+
+puts 'Creating Users and Items'
+10.times do
+  user_name = Faker::Superhero.name
+  user = User.new(
+    name: user_name,
+    email: Faker::Internet.email(name: user_name),
+    phone_number: Faker::PhoneNumber.cell_phone,
+    location: Faker::Address.full_address,
+    password: '123456'
+  )
+  user.save!
+  5.times do
+    item = Item.create!(
+      name: Faker::House.furniture,
+      description: Faker::Lorem.paragraph,
+      user: user
+    )
+  end
+end
+
+puts 'User and items created'
+puts 'Creating offerings'
+
+User.find_each do |user|
+  items = Item.where.not(user: user).sample(2)
+  items.each do |item|
+    Offering.create!(
+      offered: user.items.sample,
+      posted: item
+    )
+  end
+end
+
+puts 'Created offerings'
