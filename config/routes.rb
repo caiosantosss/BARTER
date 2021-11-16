e7a9a28f564f056a2bf7baa52402601810435e99
Vivian Bow <vivianbao1009@gmail.com>
2021-11-16 11:46:50 +0900

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'items#index'
+  resources :items, only: [:show, :new, :create] do
+    resources :offerings, only: [:new, :create]
+  end
 
+  resources :offerings, only: [:edit, :update]
 end
